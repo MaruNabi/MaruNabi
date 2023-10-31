7ceba894062f63169a5ecd5e2b193b5f528ac50d
Kim YeonMo <ymthebest57@gmail.com>
2023-10-31 21:29:33 +0900

    🐛 Fix | remove MonoBehaviour

--- Assets/Scripts/Dialog/DialogData.cs
+++ Assets/Scripts/Dialog/DialogData.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DialogData : MonoBehaviour
+public class DialogData
 {
     [SerializeField] public string talkerName { get; private set; }
 
