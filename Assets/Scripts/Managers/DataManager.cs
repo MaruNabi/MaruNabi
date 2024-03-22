0aeea90b5352ca46f69ee8c23d58bb33fec9214c
시원 <96858320+NearthYou@users.noreply.github.com>
2024-03-22 20:38:34 +0900

    🐛Fix | DataManager Bug Fix
    json 파일 내 명명 수정
    MonsterData 변수 네이밍 변경
    Dictionary Key string으로 변경

--- Assets/Scripts/Managers/DataManager.cs
+++ Assets/Scripts/Managers/DataManager.cs
@@ -6,12 +6,12 @@ using System.IO;
 
 public class DataManager
 {
-    public Dictionary<int, MonsterData> monsterDict { get; private set; } = new Dictionary<int, MonsterData>();
+    public Dictionary<string, MonsterData> monsterDict { get; private set; } = new Dictionary<string, MonsterData>();
     
     public void Init()
     {
         //Resources의 Data 폴더 산하에서 해당하는 이름에 맞는 json 데이터를 읽어온다.
-        monsterDict = LoadJson<MonsterDict, int, MonsterData>("MonsterData").MakeDict();
+        monsterDict = LoadJson<MonsterDict, string, MonsterData>("MonsterData").MakeDict();
     }
 
     Loader LoadJson<Loader, Key, Value>(string path) where Loader : ILoader<Key, Value>
