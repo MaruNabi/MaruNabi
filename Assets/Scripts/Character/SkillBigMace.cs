2e1a3c090e5fc36cd9ed1b1cd7970b520ffde532
KooKyoSeoung <201811141@sangmyung.kr>
2024-04-05 22:54:48 +0900

    💫Resource | Nabi Bullets

---
+++ Assets/Scripts/Character/SkillBigMace.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class SkillBigMace : Sword
+{
+    private void OnEnable()
+    {
+        SetSword(1.5f);
+    }
+
+    void Update()
+    {
+        
+    }
+}
