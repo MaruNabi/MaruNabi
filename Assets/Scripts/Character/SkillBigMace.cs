4fc3775396d95d23513ae66440b0937b17a63700
KooKyoSeoung <201811141@sangmyung.kr>
2024-04-08 00:10:50 +0900

    ✨Feat | Maru Skill Big Mace

--- Assets/Scripts/Character/SkillBigMace.cs
+++ Assets/Scripts/Character/SkillBigMace.cs
@@ -5,13 +5,47 @@ using DG.Tweening;
 
 public class SkillBigMace : Sword
 {
+    private Vector3 spawnPosition;
+    private Vector3 maceDistance = new Vector3(2, 10, 0);
+
     private void OnEnable()
     {
         SetSword(1.5f);
+
+        if (transform.rotation.y == 0)
+        {
+            spawnPosition.x = swordReturnPosition.transform.position.x + maceDistance.x;
+        }
+
+        else
+        {
+            spawnPosition.x = swordReturnPosition.transform.position.x - maceDistance.x;
+        }
+
+        spawnPosition.y = swordReturnPosition.transform.position.y + maceDistance.y;
     }
 
     void Update()
     {
+        AttackInstantiate();
+    }
+
+    private void OnDisable()
+    {
         
+    }
+
+    protected override void AttackInstantiate()
+    {
+        base.AttackInstantiate();
+
+        StartCoroutine(SkillMaceMovement());
+    }
+
+    private IEnumerator SkillMaceMovement()
+    {
+        transform.position = spawnPosition;
+
+        yield return null;
     }
 }
