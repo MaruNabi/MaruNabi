839adc4011187eabc3a6f8faa4bc386e18254648
시원 <96858320+NearthYou@users.noreply.github.com>
2024-07-22 01:01:22 +0900

    ✨Feat | Earth Energy Beam Warning Sign

---
+++ Assets/WarningSign.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarningSign : MonoBehaviour
+{
+    [SerializeField] private GameObject earthEnergyBeam;
+
+    private void OnEnable()
+    {
+        StartCoroutine(SpawnEarthEnergyBeam());
+    }
+
+    private IEnumerator SpawnEarthEnergyBeam()
+    {
+        yield return new WaitForSeconds(0.5f);
+        earthEnergyBeam.SetActive(true);
+        Managers.Sound.PlaySFX("Mask_Earth");
+        gameObject.SetActive(false);
+    }
+}
