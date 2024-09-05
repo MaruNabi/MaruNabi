a1ea477d116f193e934790499f08356994474af1
시원 <96858320+NearthYou@users.noreply.github.com>
2024-09-05 14:26:22 +0900

    ✨Feat | Reserve Spawn Point

--- Assets/Scripts/Environments/BoundaryLine.cs
+++ Assets/Scripts/Environments/BoundaryLine.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 using Vector2 = System.Numerics.Vector2;
 
 public class BoundaryLine : MonoBehaviour
 {
     [SerializeField] private Transform resetPoint;
+    [SerializeField] private Transform reservePoint;
+    [SerializeField] private GameObject cloud;
     
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             collision.gameObject.GetComponent<Player>().PlayerHitSpecial(Vector3.zero);
-            collision.gameObject.transform.position = resetPoint.position;
+            if (cloud.activeSelf)
+                collision.gameObject.transform.position = resetPoint.position;
+            else
+                collision.gameObject.transform.position = reservePoint.position;
         }
     }
 }
